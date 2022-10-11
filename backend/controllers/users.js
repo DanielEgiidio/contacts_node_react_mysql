1ba418d80fe4bb1c5fa50455adededc3c0332bce
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-10-11 16:02:32 -0300

    Inserção dos metodos Adição, Atualização e Deleceção no Backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -7,3 +7,49 @@ export const getUsers = (_, res) => {
     return res.status(200).json(data);
   });
 };
+
+export const addUser = (req, res) => {
+  const q =
+    "INSERT INTO users(`name`, `email`, `phone`, `data_nasc`) VALUES(?)";
+
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.phone,
+    req.body.data_nasc,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Contato criado com sucesso.");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE users SET `name` = ?, `email` = ?, `phone` = ?, `data_nasc` = ? WHERE `id` = ?";
+
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.phone,
+    req.body.data_nasc,
+  ];
+
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Contato atualizado com sucesso.");
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM users WHERE `id` = ?";
+
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Contato deletado com sucesso.");
+  });
+};
